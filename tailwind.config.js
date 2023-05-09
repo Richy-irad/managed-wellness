9785a9641a3ade0c11b03399699f78a7801d8a25
Richy-irad <richardirad@gmail.com>
2023-05-10 00:36:42 +0300

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: "#0D0D0D",
+        dark: "#2A2A2A",
+        concrete: "#F2F2F2",
+        light: "#CFCFCF",
+        "deep-blue": "#003249",
+        "sage-green": "#9EB689",
+        "warm-grey": "#807664",
+        "cool-grey": "#A7ADA8",
+        "muted-yellow": "#E2D589",
       },
     },
   },
   plugins: [],
-}
+};
