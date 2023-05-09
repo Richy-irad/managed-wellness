d3824b599458287cab874affe846bf1f26a30af4
Richy-irad <richardirad@gmail.com>
2023-05-10 00:43:17 +0300

    add settings for fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,52 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const josefinSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/static/JosefinSans-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/JosefinSans-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/JosefinSans-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/JosefinSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/JosefinSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/JosefinSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/static/JosefinSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-josefin-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${josefinSans.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
